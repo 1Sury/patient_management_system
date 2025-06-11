2a5f8468135074406a21a5baab25d5d253e53e8a
Shubhanshi <shubhanshiofficial12@gmail.com>
2025-06-11 10:58:20 +0530

    Syncing

--- patient_management/wsgi.py
+++ patient_management/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patient_management.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application() 
